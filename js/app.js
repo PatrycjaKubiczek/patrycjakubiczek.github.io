4ccbc7c23fb57e65686dd4d0d764a777be67988b
PatrycjaKubiczek <p.kubiczek@gmail.com>
2016-11-18 13:58:46 +0100

    update

--- js/app.js
+++ js/app.js
@@ -1,42 +1,35 @@
-$(document).ready(function (){
+$(document).ready(function () {
     console.log('działa');
     var nav = $('nav');
     var menu = $('.menu');
-//    Zmienna przechowująca pozycję (top) elementu o klasie menu
+    //    Zmienna przechowująca pozycję (top) elementu o klasie menu
     var position = menu.offset();
-    var top = position.top; 
+    var top = position.top;
     console.log(top);
 
-    $(window).scroll(function (){
-        console.log('scroll scroll');
+    $(window).scroll(function () {
+        //        console.log('scroll scroll');
         var scrolling = $(document).scrollTop();
-        console.log(scrolling);
-        
-        if(top < scrolling) {
+        //        console.log(scrolling);
+
+        if (top < scrolling) {
             console.log('scroll part 2')
             menu.addClass('sticky');
         } else {
             console.log('remove class');
             menu.removeClass('sticky');
         }
-    }); 
-    
-    $(window).resize(function(){
-        console.log('resizing');
-       if(menu.hasClass('sticky')){
-          console.log('has class');
-//            var menu = nav.offset();
+    });
+
+    $(window).resize(function () {
+        //        console.log('resizing');
+        if (menu.hasClass('sticky')) {
+            //          console.log('has class');
+            //            var menu = nav.offset();
             top = position.top;
-            console.log(top);
-        
+            //            console.log(top);
         } else {
             top = nav.offset().top;
-            
         }
-           
-          }); 
-
-    
-    
-    
+    });
 });
