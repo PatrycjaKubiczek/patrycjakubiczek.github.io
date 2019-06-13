eecaea8a64e6f099bf22c97ac09e45bd01c17477
patrycjakubiczek <p.kubiczek@gmail.com>
2019-06-13 21:58:12 +0200

    add HT

---
+++ projects/habit-tracker-final/precache-manifest.5aace1c84ad5e39d133f2552bc4c3bd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a493c87923fcd728c5e21942268e5ab0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f35978caf65608b5f21",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "e4f4edf03008a0e52692",
+    "url": "/static/css/main.c6ea8744.chunk.css"
+  },
+  {
+    "revision": "7f35978caf65608b5f21",
+    "url": "/static/js/2.9152f936.chunk.js"
+  },
+  {
+    "revision": "e4f4edf03008a0e52692",
+    "url": "/static/js/main.91021a17.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
